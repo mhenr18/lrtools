fc04e93108526fc9d6f37a7a20dcc873a4980430
Matthew Henry <matt.henry@me.com>
2015-12-08 20:45:02 +1000

    Initial Commit

---
+++ LRTools/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LRTools
+//
+//  Created by Matthew Henry on 8/12/2015.
+//  Copyright © 2015 Matthew Henry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
